eb2ca82cb5df05728ecaf207e25c4c9d2d1ad684
Gustavogiraldo1531 <gustavogiraldo1531@gmail.com>
2025-08-25 18:35:48 -0500

    Cambio de linea 4

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #Manejo de conflictos-archivo base
 mensajes = [
     "Estoy manejando errores en github",
-    "CONFLICTO",
+    "Aprendiendo Git y GitHub",
     "Siempre has un commit con mensaje corto"
 ]
 
 for m in mensajes:
     print(m)
+
